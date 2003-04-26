4a023436895204fe460e20383810ab73f31966d8
Daniel Wallin <daniel@boostpro.com>
2003-04-26 19:42:19 +0000

    *** empty log message ***

--- luabind/scope.hpp
+++ luabind/scope.hpp
@@ -1,3 +1,26 @@
+// Copyright (c) 2003 Daniel Wallin and Arvid Norberg
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 #ifndef LUABIND_SCOPE_HPP_INCLUDED
 #define LUABIND_SCOPE_HPP_INCLUDED
 
@@ -82,13 +105,7 @@ namespace luabind
 			return scoped_sequence(this->clone(), rhs.clone());
 		}
 	}
-/*
-	def(..),
-	def(..)
-
-	function_obj1, function_obj2
-	function_obj1.push_back(function_obj2.doclone());
-*/
+	
 	struct scope_stack
 	{
 		std::stack<int> scopes;
@@ -297,32 +314,6 @@ namespace luabind
 
 	typedef scope namespace_;
 }
-/*
-scope dwarf("dwarf");
 
-dwarf
-[
-	def(..),
-	def(..),
-
-	class<vec3>("vec3")
-		.def(constructor<float,float,float>())
-		.def(self + self)
-		.def(self - self)
-		.def(self == self)
-		,
-
-	def("getDirection", &get_direction),
-	def("getPosition", &get_position)
-];
-
-dwarf
-[
-	def(..)
-];
-
-dwarf.commit(L);
-
-*/
-#endif
+#endif // LUABIND_SCOPE_HPP_INCLUDED
 
