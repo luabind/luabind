0d09884f2b83f817826917f77d083929df6dd0c7
Daniel Wallin <daniel@boostpro.com>
2004-02-11 19:30:04 +0000

    scope update

---
+++ src/class.cpp
@@ -0,0 +1,357 @@
+// Copyright (c) 2004 Daniel Wallin and Arvid Norberg
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <luabind/lua_include.hpp>
+#include <luabind/config.hpp>
+#include <luabind/class.hpp>
+
+namespace luabind { namespace detail {
+    
+    struct class_registration : registration
+    {   
+        class_registration(char const* name);
+
+        void register_(lua_State* L) const;
+
+    	const char* m_name;
+
+		mutable std::map<const char*, detail::method_rep, detail::ltstr> m_methods;
+
+		// datamembers, some members may be readonly, and
+		// only have a getter function
+		mutable std::map<const char*, detail::class_rep::callback, detail::ltstr> m_getters;
+		mutable std::map<const char*, detail::class_rep::callback, detail::ltstr> m_setters;
+
+		// the operators in lua
+		mutable std::vector<detail::class_rep::operator_callback> m_operators[detail::number_of_operators]; 
+		mutable std::map<const char*, int, detail::ltstr> m_static_constants;
+
+		mutable std::vector<class_base::base_desc> m_bases;
+		mutable detail::construct_rep m_constructor;
+
+		void(*m_destructor)(void*);
+		void(*m_const_holder_destructor)(void*);
+
+		void*(*m_extractor)(void*);
+		const void*(*m_const_extractor)(void*);
+
+		void(*m_const_converter)(void*,void*);
+
+		void(*m_construct_holder)(void*, void*);
+		void(*m_construct_const_holder)(void*, void*);
+
+		int m_holder_size;
+		int m_holder_alignment;
+
+		LUABIND_TYPE_INFO m_type;
+		LUABIND_TYPE_INFO m_holder_type;
+		LUABIND_TYPE_INFO m_const_holder_type;
+
+#ifndef LUABIND_DONT_COPY_STRINGS
+		// the maps that contains char pointers points into
+		// this vector of strings. 
+		mutable std::vector<char*> m_strings;
+#endif
+    };
+
+    class_registration::class_registration(char const* name)
+    {
+#ifndef LUABIND_DONT_COPY_STRINGS
+        m_strings.push_back(detail::dup_string(name));
+        m_name = m_strings.back();
+#else
+        m_name = name;
+#endif
+    }
+
+    void class_registration::register_(lua_State* L) const
+    {
+        lua_pushstring(L, m_name);
+
+        detail::class_rep* crep;
+
+        detail::class_registry* r = detail::class_registry::get_registry(L);
+        // create a class_rep structure for this class.
+        // allocate it within lua to let lua collect it on
+        // lua_close(). This is better than allocating it
+        // as a static, since it will then be destructed
+        // when the program exits instead.
+        // warning: we assume that lua will not
+        // move the userdata memory.
+        lua_newuserdata(L, sizeof(detail::class_rep));
+        crep = reinterpret_cast<detail::class_rep*>(lua_touserdata(L, -1));
+
+        new(crep) detail::class_rep(
+            m_type
+            , m_name
+            , L
+            , m_destructor
+            , m_const_holder_destructor
+            , m_holder_type
+            , m_const_holder_type
+            , m_extractor
+            , m_const_extractor
+            , m_const_converter
+            , m_construct_holder
+            , m_construct_const_holder
+            , m_holder_size
+            , m_holder_alignment);
+
+        // register this new type in the class registry
+        r->add_class(m_type, crep);
+        if (!(LUABIND_TYPE_INFO_EQUAL(m_holder_type, LUABIND_INVALID_TYPE_INFO)))
+        {
+            // if we have a held type
+            // we have to register it in the class-table
+            // but only for the base class, if it already
+            // exists, we don't have to register it
+            detail::class_rep* c = r->find_class(m_holder_type);
+            if (c == 0)
+            {
+                r->add_class(m_holder_type, crep);
+                r->add_class(m_const_holder_type, crep);
+            }
+        }
+
+        // constructors
+        m_constructor.swap(crep->m_constructor);
+
+#ifndef LUABIND_DONT_COPY_STRINGS
+        assert(crep->m_strings.empty() && "Internal error");
+        crep->m_strings.swap(m_strings);
+#endif
+
+        crep->m_getters.swap(m_getters);
+        crep->m_setters.swap(m_setters);
+
+        for(int i = 0; i < detail::number_of_operators; ++i)
+            crep->m_operators[i].swap(m_operators[i]);
+
+        crep->m_static_constants.swap(m_static_constants);
+
+        // here!
+        //			crep->m_methods = m_methods;
+
+        for (std::vector<class_base::base_desc>::iterator i = m_bases.begin();
+            i != m_bases.end(); 
+            ++i)
+        {
+            detail::class_registry* r = detail::class_registry::get_registry(L);
+
+            // the baseclass' class_rep structure
+            detail::class_rep* bcrep = r->find_class(i->type);
+
+            detail::class_rep::base_info base;
+            base.pointer_offset = i->ptr_offset;
+            base.base = bcrep;
+
+            crep->add_base_class(base);
+
+            typedef std::map<const char*, detail::method_rep, detail::ltstr> methods_t;
+
+            for (methods_t::const_iterator i 
+                = bcrep->m_methods.begin()
+                ; i != bcrep->m_methods.end()
+                ; ++i)
+            {
+                detail::method_rep& m = m_methods[i->first];
+
+                typedef std::vector<detail::overload_rep> overloads_t;
+
+                for (overloads_t::const_iterator j
+                    = i->second.overloads().begin()
+                    ; j != i->second.overloads().end()
+                    ; ++j)
+                {
+                    detail::overload_rep o = *j;
+                    o.add_offset(base.pointer_offset);
+                    m.add_overload(o);
+                }
+            }
+
+            // copy base class table
+            detail::getref(L, crep->table_ref());
+            detail::getref(L, bcrep->table_ref());
+            lua_pushnil(L);
+
+            while (lua_next(L, -2))
+            {
+                lua_pushvalue(L, -2); // copy key
+                lua_insert(L, -2);
+                lua_settable(L, -5);
+            }
+
+            lua_pop(L, 2);
+        }
+
+        crep->m_methods = m_methods;
+
+        for (std::map<const char*, detail::method_rep, detail::ltstr>::iterator i 
+            = crep->m_methods.begin(); i != crep->m_methods.end(); ++i)
+        {
+            i->second.crep = crep;
+        }
+
+        // add methods
+        for (std::map<const char*, detail::method_rep, detail::ltstr>::iterator i 
+            = m_methods.begin(); i != m_methods.end(); ++i)
+        {
+            detail::getref(L, crep->table_ref());
+            lua_pushstring(L, i->first);
+            lua_pushnil(L);
+            lua_settable(L, -3);
+            lua_pop(L, 1);
+
+            crep->add_method(L, i->first, crep->m_methods[i->first]);
+            i->second.crep = crep;
+        }
+
+        m_methods.clear();
+
+        lua_settable(L, -3);
+    }
+    
+    // -- interface ---------------------------------------------------------
+
+    class_base::class_base(char const* name)
+        : scope(std::auto_ptr<registration>(
+                m_registration = new class_registration(name))
+          )
+    {
+    }
+
+    void class_base::init(
+        LUABIND_TYPE_INFO type
+        , LUABIND_TYPE_INFO holder_type
+        , LUABIND_TYPE_INFO const_holder_type
+        , void*(*extractor)(void*)
+        , const void*(*const_extractor)(void*)
+        , void(*const_converter)(void*,void*)
+        , void(*holder_constructor)(void*,void*)
+        , void(*const_holder_constructor)(void*,void*)
+        , void(*destructor)(void*)
+        , void(*const_holder_destructor)(void*)
+        , int holder_size
+        , int holder_alignment)
+    {
+        m_registration->m_type = type;
+        m_registration->m_holder_type = holder_type;
+        m_registration->m_const_holder_type = const_holder_type;
+        m_registration->m_extractor = extractor;
+        m_registration->m_const_extractor = const_extractor;
+        m_registration->m_const_converter = const_converter;
+        m_registration->m_construct_holder = holder_constructor;
+        m_registration->m_construct_const_holder = const_holder_constructor;
+        m_registration->m_destructor = destructor;
+        m_registration->m_const_holder_destructor = const_holder_destructor;
+        m_registration->m_holder_size = holder_size;
+        m_registration->m_holder_alignment = holder_alignment;
+    }
+
+    void class_base::add_getter(
+        const char* name, const boost::function2<int, lua_State*, int>& g)
+	{
+		detail::class_rep::callback c;
+		c.func = g;
+		c.pointer_offset = 0;
+#ifndef LUABIND_DONT_COPY_STRINGS
+		m_registration->m_strings.push_back(detail::dup_string(name));
+		m_registration->m_getters[m_registration->m_strings.back()] = c;
+#else
+		m_registration->m_getters[name] = c;
+#endif
+	}
+
+    void class_base::add_setter(
+        const char* name, const boost::function2<int, lua_State*, int>& s)
+    {
+        detail::class_rep::callback c;
+        c.func = s;
+        c.pointer_offset = 0;
+#ifndef LUABIND_DONT_COPY_STRINGS
+        m_registration->m_strings.push_back(detail::dup_string(name));
+        m_registration->m_setters[m_registration->m_strings.back()] = c;
+#else
+        m_registration->m_setters[name] = c;
+#endif
+    }
+
+    void class_base::add_base(const base_desc& b)
+    {
+        m_registration->m_bases.push_back(b);
+    }
+
+    void class_base::add_constructor(const detail::construct_rep::overload_t& o)
+    {
+        m_registration->m_constructor.overloads.push_back(o);
+    }
+
+    void class_base::add_method(const char* name, const detail::overload_rep& o)
+    {
+#ifdef LUABIND_DONT_COPY_STRINGS
+        detail::method_rep& method = m_registration->m_methods[name];
+        method.name = name;
+#else
+        m_registration->m_strings.push_back(detail::dup_string(name));
+        detail::method_rep& method = m_registration->m_methods[
+            m_registration->m_strings.back()];
+        method.name = m_registration->m_strings.back();
+#endif
+        method.add_overload(o);
+        method.crep = 0;
+    }
+
+#ifndef LUABIND_NO_ERROR_CHECKING
+    void class_base::add_operator(
+        int op_id,  int(*func)(lua_State*), int(*matcher)(lua_State*)
+        , void(*sig)(lua_State*, std::string&), int arity)
+#else
+    void class_base::add_operator(
+        int op_id,  int(*func)(lua_State*)
+        , int(*matcher)(lua_State*), int arity)
+#endif
+    {
+        detail::class_rep::operator_callback o;
+        o.set_fun(func);
+        o.set_match_fun(matcher);
+        o.set_arity(arity);
+
+#ifndef LUABIND_NO_ERROR_CHECKING
+
+        o.set_sig_fun(sig);
+
+#endif
+        m_registration->m_operators[op_id].push_back(o);
+    }
+
+    const char* class_base::name() const 
+    { 
+        return m_registration->m_name; 
+    }
+
+	void class_base::add_static_constant(const char* name, int val)
+	{
+		m_registration->m_static_constants[name] = val;
+	}
+	
+}} // namespace luabind::detail
+
---
+++ src/scope.cpp
@@ -0,0 +1,200 @@
+// Copyright (c) 2004 Daniel Wallin and Arvid Norberg
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <luabind/lua_include.hpp>
+#include <luabind/scope.hpp>
+#include <cassert>
+
+namespace luabind { namespace detail {
+
+    registration::registration()
+        : m_next(0)
+    {
+    }
+
+    registration::~registration()
+    {
+        delete m_next;
+    }
+
+    scope::scope()
+        : m_chain(0)
+    {
+    }
+    
+    scope::scope(std::auto_ptr<registration> reg)
+        : m_chain(reg.release())
+    {
+    }
+
+    scope::scope(scope const& other)
+        : m_chain(other.m_chain)
+    {
+        const_cast<scope&>(other).m_chain = 0;
+    }
+
+    scope::~scope()
+    {
+        delete m_chain;
+    }
+    
+    scope& scope::operator,(scope s)
+    {
+        if (!m_chain) 
+        {
+            m_chain = s.m_chain;
+            s.m_chain = 0;
+            return *this;
+        }
+        
+        for (registration* c = m_chain;; c = c->m_next)
+        {
+            if (!c->m_next)
+            {
+                c->m_next = s.m_chain;
+                s.m_chain = 0;
+                break;
+            }
+        }
+
+        return *this;
+    }
+
+    void scope::register_(lua_State* L) const
+    {
+        for (registration* r = m_chain; r != 0; r = r->m_next)
+        {
+            int n = lua_gettop(L);
+            
+            r->register_(L);
+
+            assert(n == lua_gettop(L));
+        }
+    }
+
+}} // namespace luabind::detail
+
+namespace luabind {
+
+    namespace {
+
+        struct lua_pop_stack
+        {
+            lua_pop_stack(lua_State* L)
+                : m_state(L)
+            {
+            }
+
+            ~lua_pop_stack()
+            {
+                lua_pop(m_state, 1);
+            }
+
+            lua_State* m_state;
+        };
+
+    } // namespace unnamed
+    
+    module_::module_(lua_State* L, char const* name = 0)
+        : m_state(L)
+        , m_name(name)
+    {
+    }
+
+    void module_::operator[](detail::scope s)
+    {
+        if (m_name)
+        {
+            lua_pushstring(m_state, m_name);
+            lua_gettable(m_state, LUA_GLOBALSINDEX);
+
+            if (!lua_istable(m_state, -1))
+            {
+                lua_pop(m_state, 1);
+
+                lua_newtable(m_state);
+                lua_pushstring(m_state, m_name);
+                lua_pushvalue(m_state, -2);
+                lua_settable(m_state, LUA_GLOBALSINDEX);
+            }
+        }
+        else
+        {
+            lua_pushvalue(m_state, LUA_GLOBALSINDEX);
+        }
+
+        lua_pop_stack guard(m_state);
+
+        s.register_(m_state);
+    }
+
+    struct namespace_::registration_ : detail::registration
+    {
+        registration_(char const* name)
+            : m_name(name)
+        {
+        }
+
+        void register_(lua_State* L) const
+        {
+            int n = lua_gettop(L);
+
+            assert(n >= 1);
+
+            lua_pushstring(L, m_name);
+            lua_gettable(L, -2);
+
+            if (!lua_istable(L, -1))
+            {
+                lua_pop(L, 1);
+
+                lua_newtable(L);
+                lua_pushstring(L, m_name);
+                lua_pushvalue(L, -2);
+                lua_settable(L, -4);
+            }
+
+            m_scope.register_(L);
+
+            lua_pop(L, 1);
+
+            assert(lua_gettop(L) == n);
+        }
+
+        char const* m_name;
+        scope m_scope;
+    };
+
+    namespace_::namespace_(char const* name)
+        : scope(std::auto_ptr<detail::registration>(
+              m_registration = new registration_(name)))
+    {
+    }
+
+    namespace_& namespace_::operator[](scope s)
+    {
+        m_registration->m_scope.operator,(s);        
+        return *this;
+    }
+
+} // namespace luabind
+
