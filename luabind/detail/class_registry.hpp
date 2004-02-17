d62aae21ab94bec7ba7625196731bd21cbd09594
Daniel Wallin <daniel@boostpro.com>
2004-02-17 22:16:59 +0000

    moved around some code

--- luabind/detail/class_registry.hpp
+++ luabind/detail/class_registry.hpp
@@ -24,9 +24,11 @@
 #ifndef LUABIND_CLASS_REGISTRY_HPP_INCLUDED
 #define LUABIND_CLASS_REGISTRY_HPP_INCLUDED
 
+#include <typeinfo>
+#include <map>
+
 #include <luabind/config.hpp>
 #include <luabind/open.hpp>
-#include <string>
 
 namespace luabind { namespace detail
 {
@@ -34,42 +36,9 @@ namespace luabind { namespace detail
 
 	struct class_registry
 	{
-		class_registry(lua_State* L)
-			: m_cpp_instance_metatable(create_cpp_instance_metatable(L))
-			, m_cpp_class_metatable(create_cpp_class_metatable(L))
-			, m_lua_instance_metatable(create_lua_instance_metatable(L))
-			, m_lua_class_metatable(create_lua_class_metatable(L))
-			, m_lua_function_metatable(create_lua_function_metatable(L))
-		{}
+		class_registry(lua_State* L);
 
-		static inline class_registry* get_registry(lua_State* L)
-		{
-
-#ifdef LUABIND_NOT_THREADSAFE
-
-			// if we don't have to be thread safe, we can keep a
-			// chache of the class_registry pointer without the
-			// need of a mutex
-			static lua_State* cache_key = 0;
-			static class_registry* registry_cache = 0;
-			if (cache_key == L) return registry_cache;
-
-#endif
-
-			lua_pushstring(L, "__luabind_classes");
-			lua_gettable(L, LUA_REGISTRYINDEX);
-			class_registry* p = static_cast<class_registry*>(lua_touserdata(L, -1));
-			lua_pop(L, 1);
-
-#ifdef LUABIND_NOT_THREADSAFE
-
-			cache_key = L;
-			registry_cache = p;
-
-#endif
-
-			return p;
-		}
+		static class_registry* get_registry(lua_State* L);
 
 		int cpp_instance() const { return m_cpp_instance_metatable; }
 		int cpp_class() const { return m_cpp_class_metatable; }
@@ -78,12 +47,7 @@ namespace luabind { namespace detail
 		int lua_class() const { return m_lua_class_metatable; }
 		int lua_function() const { return m_lua_function_metatable; }
 
-		void add_class(LUABIND_TYPE_INFO info, class_rep* crep)
-		{
-			// class is already registered
-			assert((m_classes.find(info) == m_classes.end()) && "you are trying to register a class twice");
-			m_classes[info] = crep;
-		}
+		void add_class(LUABIND_TYPE_INFO info, class_rep* crep);
 
 		struct cmp
 		{
@@ -99,12 +63,7 @@ namespace luabind { namespace detail
 			}
 		};
 		
-		class_rep* find_class(LUABIND_TYPE_INFO info)
-		{
-			std::map<LUABIND_TYPE_INFO, class_rep*, cmp>::iterator i = m_classes.find(info);
-			if (i == m_classes.end()) return 0; // the type is not registered
-			return i->second;
-		}
+		class_rep* find_class(LUABIND_TYPE_INFO info) const;
 
 	private:
 
