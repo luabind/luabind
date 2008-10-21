5f0ccfaff4f98e7bc386c45ac97cbde871463ed6
Daniel Wallin <daniel@boostpro.com>
2008-10-21 23:21:54 +0200

    Make using dependency() on non class return types a nop.
    It would fail with an assertion if used on functions with non-class
    return types. This was a problem for iterator_policy, which turns the
    return value into a c-closure.

--- luabind/dependency_policy.hpp
+++ luabind/dependency_policy.hpp
@@ -40,7 +40,10 @@ namespace luabind { namespace detail
 			int patient = indices[B];
 
 			object_rep* nurse = static_cast<object_rep*>(lua_touserdata(L, nurse_index));
-			assert((nurse != 0) && "internal error, please report"); // internal error
+
+			// If the nurse isn't an object_rep, just make this a nop.
+			if (nurse == 0)
+				return;
 
 			nurse->add_dependency(L, patient);
 		}
