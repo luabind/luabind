7be5add68882cb2434384b2980990be34a129b27
Daniel Wallin <daniel@boostpro.com>
2008-12-20 20:34:32 +0100

    Move LUABIND_BUILDING define from Jamroot to source files.
    It's a lot better to just define this at the top of every source file.
    It simplifies for people using other build systems.

--- src/class_registry.cpp
+++ src/class_registry.cpp
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
 
+#define LUABIND_BUILDING
+
 #include <luabind/lua_include.hpp>
 
 #include <luabind/luabind.hpp>
