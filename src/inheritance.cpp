3bb2261650765569321917949b816190b0ad00a2
Daniel Wallin <daniel@boostpro.com>
2009-11-07 04:39:20 +0100

    Add missing LUABIND_BUILDING define to inheritance.cpp.

--- src/inheritance.cpp
+++ src/inheritance.cpp
@@ -2,6 +2,8 @@
 // subject to the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define LUABIND_BUILDING
+
 #include <limits>
 #include <map>
 #include <vector>
