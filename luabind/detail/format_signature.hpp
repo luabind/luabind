098a71ee1c43eb2af5516ba462dfceff9c867f23
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-01 14:31:26 -0500

    add dummy template parameter to type_to_string for use with SFINAE/enable_if

--- luabind/detail/format_signature.hpp
+++ luabind/detail/format_signature.hpp
@@ -33,7 +33,7 @@ namespace luabind { namespace detail {
 
 LUABIND_API std::string get_class_name(lua_State* L, type_id const& i);
 
-template <class T>
+template <class T, class Enable = void>
 struct type_to_string
 {
     static void get(lua_State* L)
