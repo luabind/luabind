5ce888ab82c1fc2ea297fe5e82e84ba74833518f
Ryan Pavlik <rpavlik@iastate.edu>
2012-05-22 18:36:58 -0500

    Don't make lots of anonymous "self" and "const_self."
    This silences some warnings from operator.hpp, and uses
    the same solution as nil.hpp uses.
    
    Conflicts:
    	luabind/operator.hpp
    	src/CMakeLists.txt

--- luabind/operator.hpp
+++ luabind/operator.hpp
@@ -345,13 +345,9 @@ namespace luabind {
 
 #undef LUABIND_BINARY_OPERATOR
 
-    namespace {
-
-        LUABIND_ANONYMOUS_FIX self_type self;
-        LUABIND_ANONYMOUS_FIX const_self_type const_self;
-
-    } // namespace unnamed
-
+    // defined in operator.cpp
+    extern LUABIND_API self_type self;
+    extern LUABIND_API const_self_type const_self;
 } // namespace luabind
 
 #endif // OPERATOR_040729_HPP
