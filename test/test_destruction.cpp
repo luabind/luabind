84be85ee2a4ada8ea9c41451c7814b1e5bc26dc2
Christian Neumüller <cn00@gmx.at>
2013-06-29 14:59:21 +0200

    test_destruction.cpp: Correct copyright note.

--- test/test_destruction.cpp
+++ test/test_destruction.cpp
@@ -1,4 +1,4 @@
-// Copyright Daniel Wallin 2009. Use, modification and distribution is
+// Copyright Christian Neumüller 2013. Use, modification and distribution is
 // subject to the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
