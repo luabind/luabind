3c6e6da6e9755a68cfde743dc4b37a6defd91f9d
Christian Neumüller <cn00@gmx.at>
2013-07-07 15:56:58 +0200

    test_scope: Test unnamed classes.
    Also test that creating a module inside a table really does not create a global
    (although it is technically impossible without a string name).

--- test/test_scope.cpp
+++ test/test_scope.cpp
@@ -48,8 +48,21 @@ struct test_class2 : counted_type<test_class2>
     { return 1; }
 };
 
+struct test_class_unnamed : counted_type<test_class_unnamed>
+{
+    test_class_unnamed() {}
+    int f() { return 42; }
+};
+
+test_class_unnamed create_unnamed()
+{
+    return test_class_unnamed();
+}
+
+
 COUNTER_GUARD(test_class);
 COUNTER_GUARD(test_class2);
+COUNTER_GUARD(test_class_unnamed);
 
 } // namespace unnamed
 
@@ -92,8 +105,12 @@ void test_main(lua_State* L)
         namespace_("inner")
         [
             def("g", &g_)
-        ]
+        ],
 
+        class_<test_class_unnamed>()
+            .def(constructor<>())
+            .def("f", &test_class_unnamed::f),
+        def("create_unnamed", &create_unnamed)
     ];
 
     module(L, "test")
@@ -131,5 +148,11 @@ void test_main(lua_State* L)
 
     globals(L)["test_object"] = test;
 
+    DOSTRING(L, "assert(not inner)");
     DOSTRING(L, "assert(test_object.inner.h() == 6)");
+
+    DOSTRING(L, "assert(not test_class_unnamed)");
+    DOSTRING(L,
+        "u = test.create_unnamed()\n"
+        "assert(u:f() == 42)");
 }
