095d8382f457e07841a90d29fdcc5f8bf66fe495
Christian Neumüller <cn00@gmx.at>
2013-07-31 11:05:53 +0200

    Allow operators to return multiple values.

--- src/object_rep.cpp
+++ src/object_rep.cpp
@@ -203,8 +203,8 @@ namespace luabind { namespace detail
                   if (is_unary) // remove trailing nil
                     lua_remove(L, 3);
 
-                  lua_call(L, nargs, 1);
-                  return 1;
+                  lua_call(L, nargs, LUA_MULTRET);
+                  return lua_gettop(L);
               }
           }
           object_rep* inst = get_instance(L, 1);
@@ -219,8 +219,7 @@ namespace luabind { namespace detail
           }
           lua_pushfstring(L, "%sclass %s: no %s operator defined.",
               const_s, cls_name, op_name);
-          lua_error(L);
-          return 0;
+          return lua_error(L);
       }
 
     } // namespace unnamed
