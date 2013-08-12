07e0438542f2ef27b480fb7a967514a65bf9b08c
Christian Neumüller <cn00@gmx.at>
2013-08-12 12:48:07 +0200

    Use enable_if to keep tag_function working.
    Also add signature_aux helper class template which will be used to simplify
    the implementation of tag_function somewhat and to introduce support for
    boost::function and std::function.

--- luabind/detail/deduce_signature.hpp
+++ luabind/detail/deduce_signature.hpp
@@ -8,7 +8,10 @@
 #  include <luabind/detail/most_derived.hpp>
 
 #  include <boost/function_types/components.hpp>
+#  include <boost/function_types/is_callable_builtin.hpp>
+#  include <boost/function_types/is_member_function_pointer.hpp>
 #  include <boost/type_traits/add_reference.hpp>
+#  include <boost/utility/enable_if.hpp>
 
 namespace luabind { namespace detail {
 
@@ -16,16 +19,31 @@ namespace mpl = boost::mpl;
 namespace fty = boost::function_types;
 
 template <class F>
-fty::components<F> deduce_signature(F, ...)
+struct signature_aux
 {
-    return fty::components<F>();
+    typedef F type;
+};
+
+template <class F>
+typename boost::enable_if<
+    fty::is_callable_builtin<typename signature_aux<F>::type>,
+    fty::components<typename signature_aux<F>::type> >::type
+deduce_signature(F const&, ...)
+{
+    return fty::components<typename signature_aux<F>::type>();
 }
 
 template <class F, class Wrapped>
-fty::components<F, boost::add_reference<most_derived<mpl::_1, Wrapped> > >
+typename boost::enable_if<
+    fty::is_member_function_pointer<typename signature_aux<F>::type>,
+    fty::components<
+        typename signature_aux<F>::type,
+        boost::add_reference<most_derived<mpl::_1, Wrapped> > > >::type
 deduce_signature(F, Wrapped*)
 {
-    return fty::components<F, boost::add_reference<most_derived<mpl::_1, Wrapped> > >();
+    return fty::components<
+        typename signature_aux<F>::type,
+        boost::add_reference<most_derived<mpl::_1, Wrapped> > >();
 }
 
 } } // namespace luabind::detail
