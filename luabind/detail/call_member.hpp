5ddff28a8e1bf41278569181f2885b5221517f02
Benjamin-L <benjamin.fik.lee@gmail.com>
2014-03-18 19:51:17 -0700

    Added another BOOST_NOEXCEPT_IF(false) to proxy_member_void_caller

--- luabind/detail/call_member.hpp
+++ luabind/detail/call_member.hpp
@@ -233,7 +233,7 @@ namespace luabind
 					rhs.m_called = true;
 				}
 
-				~proxy_member_void_caller()
+				~proxy_member_void_caller() BOOST_NOEXCEPT_IF(false)
 				{
 					if (m_called) return;
 
